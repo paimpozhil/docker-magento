e741ebc2cb9b66d99788df6d964df43b90686892
paimpozhil <paimpozhil@gmail.com>
2014-08-16 14:49:41 -0400

    Wait for mysql server to come up before creating db

--- start.sh
+++ start.sh
@@ -6,7 +6,13 @@ sed -i "s/<username\/>/<username>user<\/username>/" /var/www/app/etc/config.xml
 sed -i "s/<password\/>/<password>password<\/password>/g" /var/www/app/etc/config.xml
 
 echo "Creating the magento database..."
-echo "create database magento" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT" 
+
+echo "create database magento" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
+
+while [ $? -ne 0 ]; do
+        echo "create database magento" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
+        echo "show tables" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT" magento
+done
 
 echo "Adding Magento Caching"
 
