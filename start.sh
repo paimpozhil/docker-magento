36b76e5c21d19538f790763e1434556205d55263
paimpozhil <paimpozhil@gmail.com>
2014-08-16 16:47:00 -0400

    fix startup

--- start.sh
+++ start.sh
@@ -13,11 +13,11 @@ sed -i "s/<password\/>/<password>password<\/password>/g" /var/www/app/etc/config
 
 echo "Creating the magento database..."
 
-echo "create database magento" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
+echo "create database magento" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h db -P "$DB_PORT_3306_TCP_PORT"
 
 while [ $? -ne 0 ]; do
-        echo "create database magento" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT"
-        echo "show tables" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h "$DB_PORT_3306_TCP_ADDR" -P "$DB_PORT_3306_TCP_PORT" magento
+        echo "create database magento" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h db -P "$DB_PORT_3306_TCP_PORT"
+        echo "show tables" | mysql -u "$DB_ENV_USER" --password="$DB_ENV_PASS" -h db -P "$DB_PORT_3306_TCP_PORT" magento
 done
 
 echo "Adding Magento Caching"
